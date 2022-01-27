0beb8a1db89309ce913451677ab71da66af4a87b
Ethan Largent <largenet@flip3.engr.oregonstate.edu>
2022-01-27 13:19:17 -0800

    completed assignment

--- cs325/homework2/knapsack.cpp
+++ cs325/homework2/knapsack.cpp
@@ -44,12 +44,8 @@ struct results dyn_prog_ks(int* val, int* wt, int w, int n){
         max_arr[0][i] = 0;
     for(int i = 1; i <= n; i++)
         for(int j = 1; j <= w; j++){
-            if(wt[i - 1] <= j){
-                if(val[i - 1] + max_arr[j - wt[i - 1]][i - 1] > max_arr[j][i - 1])
-                    max_arr[j][i] = val[i - 1] + max_arr[j - wt[i - 1]][i-1];
-                else
-                    max_arr[j][i] = max_arr[j][i - 1];
-            }
+            if(wt[i - 1] <= j && val[i - 1] + max_arr[j - wt[i - 1]][i - 1] > max_arr[j][i - 1])
+                max_arr[j][i] = val[i - 1] + max_arr[j - wt[i - 1]][i-1];
             else
                 max_arr[j][i] = max_arr[j][i - 1];
         }
@@ -96,7 +92,7 @@ int main(int argc, char** argv){
 //Print Results
         printf("\n\n == N:%5d | W:%5d | Rec time:%5.2f | DP time:%5.2f | max Rec:%5d | max DP:%5d\n",
                 n, w, rec_results.time , dyn_prog_results.time , rec_results.max , dyn_prog_results.max);
-//Print Elements
+//Print Elements(optional)
 /*
         cout << "    Elements: n:  wt - val" << endl;
         for(int i = 0; i < n; i++)
